9389101de7cbeb5a0d8f5bfceb42270287414654
dvPauloSouza <61100415+dvPauloSouza@users.noreply.github.com>
2026-04-08 00:11:12 -0300

    upgrade of location

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -4,8 +4,8 @@ class AppConstants {
   AppConstants._();
 
   static const LatLng testUserFallbackLocation = LatLng(
-    -23.66281095728693,
-    -46.72949808485587,
+    -23.66281095728693, // 23°39'46.1"S 46°43'46.2"W
+    -46.72949808485587, // Coordenadas Fatec ZS
   );
 
   static const double openRequestsRadiusKm = 25;
