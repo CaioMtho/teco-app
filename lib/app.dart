5c2216f423c6319318e16d0e34a7e5de2065b46a
KaykyFAlves <104697237+KaykyFAlves@users.noreply.github.com>
2026-05-06 21:41:41 -0300

    feat/settings

--- lib/app.dart
+++ lib/app.dart
@@ -4,19 +4,16 @@ import 'features/requests/presentation/pages/requests_map_page.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
+  
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'TECO',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: const Color(0xFF145CFF),
-          brightness: Brightness.light,
-        ),
-        useMaterial3: true,
-      ),
+      theme: ThemeData.light(),
+      darkTheme: ThemeData.dark(),
+      themeMode: ThemeMode.dark,
       home: const RequestsMapPage(),
     );
   }
